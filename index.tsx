f7c5832930b81f3975f66d01c52571ad54f20cb1
Sabana <sabanaquestexpedition.com>
2026-01-02 20:24:28 +0100

    up

--- index.tsx
+++ index.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
 
-function index() {
+
+function Index() {
   return (
-    <div>
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Welcome to My App</h1>
+      <p>
+        This is a sample index page. You can use this section to introduce
+        your application or show important information.
+      </p>
 
-
-     <p>hello world</p> 
-     
+      <button
+        onClick={() => alert('Button clicked!')}
+        style={{
+          padding: '10px 16px',
+          backgroundColor: '#0070f3',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '4px',
+          cursor: 'pointer',
+        }}
+      >
+        Click Me
+      </button>
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
